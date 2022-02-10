ea702d15b85080c299546b4bb48d7c7be6f73617
Jonas Duehring <duehring@elan-ev.de>
2022-02-10 16:56:57 +0100

    initial setup

---
+++ customElanBundle.php
@@ -0,0 +1,9 @@
+namespace KimaiPlugin\CustomElanBundle;
+
+use App\Plugin\PluginInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class YourBundle extends Bundle implements PluginInterface
+{
+}
